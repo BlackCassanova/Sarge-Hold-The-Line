afe4a63c9d749899154df0cb1bcf494af8f5aa9a
BlackCassanova <37342704+BlackCassanova@users.noreply.github.com>
2020-02-11 00:06:43 +0530

    1st Git
    >Changed game to side scroller
    >Added Rigidbody Components
    >Created Enemy Prefab
    >Created Player Building
    >Created Script for Enemy Shooting

---
+++ Sarge! Hold The Line/Assets/Scripts/Enemy1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour
+{
+
+    public float speed;
+    public float stopDistance;
+    public float retreatDistance;
+
+    public Transform player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
