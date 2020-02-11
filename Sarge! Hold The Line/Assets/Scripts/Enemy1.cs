0f9e6f8975610610213aec7cda59e5f6ebc4410f
BlackCassanova <37342704+BlackCassanova@users.noreply.github.com>
2020-02-11 16:05:25 +0530

    2nd Git
    >Created Enemy Script
    >Enemy Moves towards Player
    >Enemy shoots bullets
    >Particle system deployed in progess work
    >Color pallete decided
    >Changed enemy art

--- Sarge! Hold The Line/Assets/Scripts/Enemy1.cs
+++ Sarge! Hold The Line/Assets/Scripts/Enemy1.cs
@@ -9,18 +9,46 @@ public class Enemy1 : MonoBehaviour
     public float stopDistance;
     public float retreatDistance;
 
-    public Transform player;
-
+    private float timebetweenshots;
+    public float starttimebetweenshots;
+    
+    public GameObject bullet;
+    private Transform player;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        timebetweenshots =starttimebetweenshots;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector2.Distance(transform.position, player.position) > stopDistance)
+        {
+        	transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+             
+        }
+
+        else if (Vector2.Distance(transform.position, player.position) < stopDistance && Vector2.Distance(transform.position, player.position) > retreatDistance )
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, (-speed/2) * Time.deltaTime);
+        }
+
+        if (timebetweenshots <=0)
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity );
+            timebetweenshots = starttimebetweenshots; 
+        }
+        else
+        {
+            timebetweenshots -= Time.deltaTime;
+        }
     }
 }
